d245dcb78489b13f75a1e4da28bdba6d79d52713
4jamesccraven <4jamesccraven@gmail.com>
2024-06-06 17:18:52 -0400

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(name='mdkev',
+      version='1.0',
+      packages=find_packages(),
+      scripts=['mkdev.py', 'mkd_config.py'])
